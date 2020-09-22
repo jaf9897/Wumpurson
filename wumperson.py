13a282d025faf74da858ab69e0283a70cec6b1e5
Jojo <jaf9897@rit.edu>
2020-09-22 19:43:24 -0400

    first commit

---
+++ wumperson.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+import random
+import asyncio
+
+
+TOKEN = 'NTUwNTAyNjgwNTM2MDIzMDQx.D1jiQQ.Y9f_MmsbsZcP8cdSVEaw18CFPyo'
+blakes = open("blake.txt", "r")
+proverbs = blakes.read().split('^')
+blakes.close()
+mentions = dict()
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+@bot.command()
+async def person(ctx):
+    print("person invoked")
+    msg = random.choice(proverbs)
+    await ctx.send(msg)
+    await asyncio.sleep(2)
+    await ctx.send("So it is written.")
+
+@bot.command()
+async def friends(ctx):
+    try:
+        channel = ctx.author.voice.channel
+        await ctx.send("Initiating conflict resolution program.")
+        await ctx.send("Use !resolved to terminate conflict resolution.")
+        player = await channel.connect()
+        player.play(discord.FFmpegPCMAudio("friends.mp3"), after=lambda: print('done'))
+        while player.is_playing():
+            await asyncio.sleep(1)  # Will leave when the entire song is finished
+        player.stop()
+        await player.disconnect()
+    except AttributeError:
+        await ctx.send("Must be in a voice channel to resolve conflicts.")
+
+@bot.command()
+async def resolved(ctx):
+    try:
+        for x in bot.voice_clients:
+            return await x.disconnect()
+        ctx.send("Conflict successfully resolved.")
+    except:
+        ctx.send("No conflict resolution currently active.")
+
+"""
+React to emoji being added to a message
+This won't do anything because that emoji doesn't exist here but all we'd have to do is change the "20" to whatever
+emoji we'd want
+"""
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    channel = reaction.message.channel
+    emoji = reaction.emoji
+    author = reaction.message.author
+    if isinstance(emoji, discord.Emoji):
+        name = emoji.name
+    elif isinstance(emoji, str):
+        name = emoji
+    else:
+        raise ValueError("Unknown emoji of type:", type(emoji))
+
+    if author == bot.user and name == "20":
+        await channel.send("There is nothing more small brained than small braining a machine, you coward.")
+
+
+bot.run(TOKEN)
